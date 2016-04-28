abe5369fd7e70530ba2d7e9b4dd87237c28736ce
Joshua Hassler <joshua_hassler@student.uml.edu>
2016-04-28 13:33:21 -0400

    :memo: Updated licensing in files

--- battery.cpp
+++ battery.cpp
@@ -4,6 +4,23 @@
 
 // Copyright 2016 Joshua Hassler
 
+/*
+ This file is part of CDH test assignment.
+
+ CDH test assignmen is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ CDH test assignmen is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with CDH test assignmen.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "./battery.h"
 
 unsigned int NUMBER_OF_DRAINS = 10;  // Set to how many times to drain batteries
